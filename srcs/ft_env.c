f9274754400a86ee7fd9a90768abb4eaab043691
timur <tim.sharapov1@gmail.com>
2021-11-24 02:56:23 +0300

    env builtin

---
+++ srcs/ft_env.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_env.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: timur <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/24 02:50:40 by timur             #+#    #+#             */
+/*   Updated: 2021/11/24 02:50:44 by timur            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	ft_env(char **args, int fd, char **c_env)
+{
+	int	i;
+
+	if (args[1] != NULL)
+	{
+		ft_putendl_fd("env: Arguments are not allowed", fd);
+		return (1);
+	}
+	i = 0;
+	while (c_env[i] != NULL)
+	{
+		ft_putendl_fd(c_env[i], fd);
+		i++;
+	}
+	return (1);
+}
